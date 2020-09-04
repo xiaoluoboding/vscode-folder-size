25b1cbacb70da2e6d8498c91efee93867ab6912d
xiaoluoboding <xiaoluoboding@gmail.com>
2020-09-04 17:00:31 +0800

    feat: add countup recursive directory file size

--- src/extension.ts
+++ src/extension.ts
@@ -9,7 +9,10 @@ import { getHumanReadableSize } from './utils';
 
 const { window, workspace } = vscode;
 
-let myStatusBarItem: vscode.StatusBarItem;
+let sbi: vscode.StatusBarItem;
+let oc: vscode.OutputChannel;
+let fsp: vscode.FileSystemProvider;
+
 let [fileSize, dirSize, dirInfo] = ['', '', { total: 0, count: 0 }];
 
 export function activate({ subscriptions }: vscode.ExtensionContext) {
@@ -17,77 +20,86 @@ export function activate({ subscriptions }: vscode.ExtensionContext) {
 	// item is selected
 	const myCommandId = 'sample.showSelectionCount';
 	subscriptions.push(vscode.commands.registerCommand(myCommandId, () => {
-		vscode.window.showInformationMessage(`Folder has ${dirInfo.count} files with a total of [${dirSize}]`);
+		window.showInformationMessage(`Folder has ${dirInfo.count} files with a total of [${dirSize}]`);
 	}));
 
+	// create outputChannel
+	oc = window.createOutputChannel('project-size');
+
 	// create a new status bar item that we can now manage
-	myStatusBarItem = window.createStatusBarItem(vscode.StatusBarAlignment.Left, 1);
-	myStatusBarItem.command = myCommandId;
-	subscriptions.push(myStatusBarItem);
+	sbi = window.createStatusBarItem(vscode.StatusBarAlignment.Left, 1);
+	sbi.command = myCommandId;
+	subscriptions.push(sbi);
+
+	// file system watcher
+	// const fileSystemWatcher = workspace.createFileSystemWatcher("**/*.{*}");
+	// fileSystemWatcher.onDidChange(uri => onResourceChange(uri), subscriptions);
+	// fileSystemWatcher.onDidCreate(uri => onResourceChange(uri), subscriptions);
+	// fileSystemWatcher.onDidDelete(uri => onResourceChange(uri));
+	// subscriptions.push(fileSystemWatcher);
 
 	// register some listener that make sure the status bar 
 	// item always up-to-date
-	// subscriptions.push(window.onDidChangeActiveTextEditor(updateStatusBarItem));
-	// subscriptions.push(window.onDidChangeTextEditorSelection(updateStatusBarItem));
 	subscriptions.push(workspace.onDidChangeTextDocument(e => getFileSize(e.document)));
 	subscriptions.push(workspace.onDidOpenTextDocument(getFileSize));
-
-	// update status bar item once at start
-	// updateStatusBarItem();
 }
-
-// function updateStatusBarItem(): void {
-// 	const n = getNumberOfSelectedLines(vscode.window.activeTextEditor);
-// 	if (n > 0) {
-// 		myStatusBarItem.text = `$(megaphone) ${n} line(s) selected`;
-// 		myStatusBarItem.show();
-// 	} else {
-// 		myStatusBarItem.hide();
-// 	}
-// }
-
-// function getNumberOfSelectedLines(editor: vscode.TextEditor | undefined): number {
-// 	let lines = 0;
-// 	if (editor) {
-// 		lines = editor.selections.reduce((prev, curr) => prev + (curr.end.line - curr.start.line), 0);
-// 	}
-// 	return lines;
-// }
 
 async function getFileSize(doc: vscode.TextDocument): Promise<void>  {
 	try {
 		const fsStat = await workspace.fs.stat(doc.uri);
+		// oc.clear();
+		dirInfo = { total: 0, count: 0 };
+		await getFolderSize();
+
+		// window.showInformationMessage(JSON.stringify(fsStat));
+
+		// oc.appendLine(JSON.stringify(fsStat));
+		// oc.appendLine(fsStat.size + '');
+
+		// oc.show();
 
 		if (fsStat.size > 0) {
 			fileSize = getHumanReadableSize(fsStat.size);
 			// window.showInformationMessage(`${fileSize}`);
-			myStatusBarItem.text = `$(file-code) ${fileSize}`;
-			myStatusBarItem.show();
-			getDirSize();
+			sbi.text = `$(file-code) ${fileSize} | $(file-directory) ${dirSize}`;
+			sbi.show();
 		}
 	} catch {
-		myStatusBarItem.hide();
+		sbi.hide();
 	}
 }
 
-
 async function countAndTotalOfFilesInFolder(folder: vscode.Uri): Promise<{ total: number, count: number }> {
-	let total = 0;
-	let count = 0;
+	
+	for (const [name, type] of await workspace.fs.readDirectory(folder)) {
 
-	for (const [name, type] of await vscode.workspace.fs.readDirectory(folder)) {
 		if (type === vscode.FileType.File) {
+			// oc.appendLine(JSON.stringify(name));
+			// oc.appendLine(JSON.stringify(folder));
 			const filePath = posix.join(folder.path, name);
-			const stat = await vscode.workspace.fs.stat(folder.with({ path: filePath }));
-			total += stat.size;
-			count += 1;
+			const stat = await workspace.fs.stat(folder.with({ path: filePath }));
+			dirInfo.total += stat.size;
+			dirInfo.count += 1;
+		}
+
+		if (type === vscode.FileType.Directory) {
+			// ignore below directory
+			if (/node_modules|.git/.test(name)) continue; 
+
+			const folderPath = posix.join(folder.path, name);
+			const folderUri = folder.with({ path: folderPath });
+
+			// oc.appendLine(JSON.stringify(name) + ': ' + JSON.stringify(folderUri));
+			// oc.appendLine(JSON.stringify(dirInfo));
+
+			await countAndTotalOfFilesInFolder(folderUri);
 		}
 	}
 
-	return { total, count };
+	return dirInfo;
 }
 
-async function getDirSize () {
+async function getFolderSize (): Promise<any> {
 	if (!window.activeTextEditor) {
 		return window.showInformationMessage('Open a file first');
 	}
@@ -96,16 +108,16 @@ async function getDirSize () {
 	const folderPath = posix.dirname(fileUri.path);
 	const folderUri = fileUri.with({ path: folderPath });
 
-	dirInfo = await countAndTotalOfFilesInFolder(folderUri);
+	await countAndTotalOfFilesInFolder(folderUri);
+	dirSize = getHumanReadableSize(dirInfo.total);
+
 	/* open new text document */
 	// const doc = await workspace.openTextDocument({ content: `${info.count} files in ${folderUri.toString(true)} with a total of ${info.total} bytes` });
 	// vscode.window.showTextDocument(doc, { viewColumn: vscode.ViewColumn.Beside });
 
-	if (dirInfo) {
-		dirSize = getHumanReadableSize(dirInfo.total);
-		// window.showInformationMessage(`${info.count} files in ${folderUri.toString(true)} with a total of ${dirSize}`);
-		// window.showInformationMessage(`${dirInfo.count} files with a total of ${dirInfo.total} Bytes [${dirSize}]`);
-		// myStatusBarItem.text = `$(file-directory) ${dirSize}`;
-		// myStatusBarItem.show();
-	}
+	return dirSize;
 }
+
+// function onResourceChange (uri: vscode.Uri) {
+// 	window.showInformationMessage(JSON.stringify(uri));
+// }
